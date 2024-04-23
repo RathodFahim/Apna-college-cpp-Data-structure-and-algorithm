f981d24e3f4f58a26ad8a5b5629ab22e09190a8f
RathodFahim <rathodrathod75850@gmail.com>
2024-04-24 00:03:57 +0530

    print average marks of student

---
+++ c++ lec-1/print-AVG-mark.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+   int main() {
+    int maths, english, sci;
+
+    cout << "Enter the marks of maths: ";
+    cin >> maths;
+
+    cout << "Enter the marks of english: ";
+    cin >> english;
+
+    cout << "Enter the marks of sci: ";
+    cin >> sci;
+
+    int avg = (maths + english + sci) / 3;
+
+    cout<<"Average marks of maths, english and sci = " <<avg;
+
+    return 0;
+   }
