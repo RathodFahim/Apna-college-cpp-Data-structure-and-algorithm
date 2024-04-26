b10488fa8116d05d0b72180265850a533870d9fb
RathodFahim <rathodrathod75850@gmail.com>
2024-04-26 22:56:05 +0530

    implicit typecasting

---
+++ c++ lec-3/implicit.cpp
@@ -0,0 +1,16 @@
+// Implicit typecasting
+
+#include <iostream>
+using namespace std;
+
+    int main() {
+        cout << 10/3 << endl; // 3
+        cout << 10.0/3 << endl; // 3.33333
+        cout << 10/3.0 << endl; // 3.33333
+    
+    
+        //Predict the output (Practice QS)
+        cout << 23.5 + 2 + 'A' << endl;
+
+        return 0;
+    }
