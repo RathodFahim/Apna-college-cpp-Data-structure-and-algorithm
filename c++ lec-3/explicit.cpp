bf171996d21786efff2b43b9fbb431f71679c08e
RathodFahim <rathodrathod75850@gmail.com>
2024-04-26 22:56:23 +0530

    explicit typecasting

---
+++ c++ lec-3/explicit.cpp
@@ -0,0 +1,15 @@
+// Expliit typecasting 
+
+#include <iostream>
+using namespace std;
+
+   int main() {
+         cout << (float)10/3 << endl; // 3.33333
+         cout << 10/(float)3 << endl; // 3.33333
+         cout << (int)('A') << endl; // 65
+
+         //Predict the output (Practice QS)
+         cout << bool(3) + 2 << endl;
+    
+         return 0;
+   }
