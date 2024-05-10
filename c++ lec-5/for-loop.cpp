b4cde0250a328035d63be5b0ee49d5bed6aafafd
RathodFahim <rathodrathod75850@gmail.com>
2024-05-10 21:59:46 +0530

    for-loop in c++

---
+++ c++ lec-5/for-loop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+
+    for(num=1; num<=5; num++) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    //Apna college 5 times using for loop
+
+    for(int i=1; i<=5; i++) {
+        cout << "Apna college" << endl;
+    }
+}
