73537d098e323478002136c00ea170411b153e96
RathodFahim <rathodrathod75850@gmail.com>
2024-05-18 19:29:08 +0530

    Assignment Question-1

---
+++ c++ lec-7/Assignment/Qs-1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+bool isPalindrome(int num) {
+    int reversedNum = 0;
+    int originalNum = num;
+
+    while (num > 0) {
+        reversedNum = reversedNum * 10 + num % 10;
+        num /= 10;
+    }
+
+    return originalNum == reversedNum;
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (isPalindrome(num)) {
+        cout << num << " is a palindrome." << endl;
+    } else {
+        cout << num << " is not a palindrome." << endl;
+    }
+
+    return 0;
+}
