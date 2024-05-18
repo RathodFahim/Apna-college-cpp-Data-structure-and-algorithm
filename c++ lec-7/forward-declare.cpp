9891cc2b953ba1a1a1741c9140bb88b811b671e2
RathodFahim <rathodrathod75850@gmail.com>
2024-05-18 19:28:48 +0530

    function declaration

---
+++ c++ lec-7/forward-declare.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void sayHello(); // function declaration
+
+int main() {
+      sayHello(); // function call
+      cout << "Work Done \n";
+}
+
+void sayHello() {
+    cout << "Hello :)\n";
+}
