432620b8b9dd568c4d4d4b929a34d2dc0f2ec798
RathodFahim <rathodrathod75850@gmail.com>
2024-05-18 19:28:31 +0530

    Function overloading

---
+++ c++ lec-7/func-overload.cpp
@@ -0,0 +1,31 @@
+// Function Overloading
+
+#include <iostream>
+using namespace std;
+
+int sum(int a, int b) {
+    cout << a + b << endl;
+    return a + b;
+}
+
+double sum(double a, double b) {
+    cout << a + b << endl;
+    return a + b;
+}
+
+int sum(int a, int b, int c) {
+    cout << a + b + c << endl;
+    return a + b + c;
+}
+
+double sum(double a, double b, double c) {
+    cout << a + b + c << endl;
+    return a + b + c;
+}
+
+int main() {
+    sum(2, 3);
+    sum(2.4, 3.5);
+    sum(2, 3, 4);
+    sum(2.4, 3.5, 4.6);
+}
