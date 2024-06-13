eb6e4a1dc2ec74deecc35d044b69b10074f4d0e4
RathodFahim <rathodrathod75850@gmail.com>
2024-06-13 22:51:41 +0530

    Binary to Decimal code

--- c++ lec-8/BinToDec.cpp
+++ c++ lec-8/BinToDec.cpp
@@ -4,8 +4,23 @@ using namespace std;
 void BinToDec(int binNum) {
     int n = binNum;
     int decNum = 0;
+    int pow = 1;
+
+    while(n > 0) {
+        int lastDigit = n % 10;
+        decNum += lastDigit * pow;
+        pow *= 2;
+        n /= 10;
+    }
+
+    cout << decNum << endl;
 }
 
 int main() {
+    int binNum;
+    cin >> binNum;
 
+    BinToDec(binNum);
+
+    return 0;
 }
