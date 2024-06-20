831ea0b0560dbb7b06815f9516b09fbec7c169dd
RathodFahim <rathodrathod75850@gmail.com>
2024-06-20 22:34:35 +0530

    input of an array by user

---
+++ c++_lec-10/input-of-array.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the siZe of the array " << "\n";
+    cin >> n;
+
+    int arr[n];
+
+    //input 
+    for(int i=0; i<n; i++) {
+        cin >> arr[i];
+    }
+
+    //output
+    for(int i=0; i<n; i++) {
+        cout << arr[i] << ",";
+    }
+    cout << endl;
+}
