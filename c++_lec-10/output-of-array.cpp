f5b2876aa39fc6e99475d505ee8dd0cfa1920950
RathodFahim <rathodrathod75850@gmail.com>
2024-06-20 22:18:44 +0530

    output of array

--- c++_lec-10/output-of-array.cpp
+++ c++_lec-10/output-of-array.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main() {
     int arr[] = {12, 43, 65, 7, 87};
-    int len = sizeof(arr) / sizeof(int);
+    int n = sizeof(arr) / sizeof(int);
 
     // output of array
-    for(int i=0; i<=len; i++) {
+    for(int i=0; i<=n; i++) {
         cout << arr[i] << " ";
     }
     cout << endl;
