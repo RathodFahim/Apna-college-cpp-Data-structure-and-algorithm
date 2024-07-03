708d62e357f7e6ee702a154c4bf498c6d82a1109
RathodFahim <rathodrathod75850@gmail.com>
2024-07-03 19:08:07 +0530

    Output an Array

---
+++ c++_lec-10(Part-1)/output-of-array.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {12, 43, 65, 7, 87};
+    int n = sizeof(arr) / sizeof(int);
+
+    // output of array
+    for(int i=0; i<n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
