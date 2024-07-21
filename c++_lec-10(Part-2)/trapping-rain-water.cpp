1898cfa343736900457e5b187c546c679d8ae315
RathodFahim <rathodrathod75850@gmail.com>
2024-07-21 12:05:46 +0530

    Trapping rain water Problem

--- c++_lec-10(Part-2)/trapping-rain-water.cpp
+++ c++_lec-10(Part-2)/trapping-rain-water.cpp
@@ -3,16 +3,26 @@ using namespace std;
 
 void trappingRainWater(int *height, int n) {
     int leftMax[20000], rightMax[20000];
-    leftMax[0] = INT32_MIN;
-    rightMax[n-1] = INT32_MIN;
+    leftMax[0] = height[0];
+    rightMax[n-1] = height[n-1];
 
     for(int i=1; i<n; i++) {
         leftMax[i] = max(leftMax[i-1], height[i-1]);
     }
 
-    for(int i=n-1; i>=0; i--) {
+    for(int i=n-2; i>=0; i--) {
         rightMax[i] = max(rightMax[i+1], height[i+1]);
     }
+
+    int water = 0;
+    for(int i=1; i<n; i++) {
+        int currWater = min(leftMax[i], rightMax[i]) - height[i];
+        if(currWater > 0) {
+            water += currWater;
+        }
+    }
+
+    cout << "Trapped water: " << water << endl;
 }
 
 int main() {
