c70ff5c6589cc0d536377a3922f9ca831c663584
RathodFahim <rathodrathod75850@gmail.com>
2024-09-28 20:43:45 +0530

    find the 2's in 2D array

--- c++_lec-12/assignment/Q1.cpp
+++ c++_lec-12/assignment/Q1.cpp
@@ -1,24 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int findtwo(int arr[][5], int n, int m, int count) {
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<m; j++) {
-            if(arr[i][j] == 2) {
-              count++;  
+int findseven(int arr[][5], int n, int m) {
+    int count = 0;  // Initialize count inside the function
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            if(arr[i][j] == 2) {  // Correct array indexing
+                count++;  
             }
         }
     }
-    
     return count;
 }
 
-int main()
-{
+int main() {
     int arr[4][5] = {{1,2,3,4,5}, {2,3,5,6,8}, {2,4,1,6,9}, {5,6,43,21,2}};
     int n = 4;
     int m = 5;
-    int count = 0;
     
-    cout << "the 2 is appear in " << findtwo(arr, n, m, count) << " times " ;
+    cout << "The number 2 appears " << findseven(arr, n, m) << " times.";
 }
