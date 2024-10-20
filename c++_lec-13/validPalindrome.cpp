92ef45ecf0c7e4103f4f5be367b35a1f0d61eb2b
RathodFahim <rathodrathod75850@gmail.com>
2024-10-20 20:04:31 +0530

    Valid Palindrome or not

---
+++ c++_lec-13/validPalindrome.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+bool isPalindrome(char word[], int n) {
+   int st = 0, end = n-1;
+   while(st < end) {
+       if(word[st] != word[end]) {
+           cout << "Not a palindrome" << endl;
+           return false;
+       }
+       st++;
+       end--;
+   }
+
+   cout << "Palindrome" << endl;
+   return true;
+}
+
+int main() {
+    char word[] = "apple";
+    isPalindrome(word, strlen(word));
+}
