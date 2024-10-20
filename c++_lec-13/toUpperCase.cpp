59fc85219246250340fc5a03f5a74fc3c6ec7fa8
RathodFahim <rathodrathod75850@gmail.com>
2024-10-20 15:40:21 +0530

    To Upper Case

---
+++ c++_lec-13/toUpperCase.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void toUpper(char word[], int n) {
+    for(int i=0; i<n; i++) {
+        char ch = word[i];
+        if()
+    }
+}
+
+int main() {
+    char word[] = "ApPle";
+
+}
