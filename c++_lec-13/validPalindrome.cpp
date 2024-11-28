00d952df07b865aae4e7282fbddfa71d1769a714
RathodFahim <rathodrathod75850@gmail.com>
2024-11-28 19:31:05 +0530

    Valid palindrome or not

--- c++_lec-13/validPalindrome.cpp
+++ c++_lec-13/validPalindrome.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 // palindrome is a word that reads the same backward as forward
 bool isPalindrome(char word[], int n) {
+    // if the word is empty or has only one character
    int st = 0, end = n-1;
    while(st < end) {
        if(word[st] != word[end]) {
