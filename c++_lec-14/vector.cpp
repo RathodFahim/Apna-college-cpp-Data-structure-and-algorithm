c6465e6b52621f40c040566e7da6e211dee0346d
RathodFahim <rathodrathod75850@gmail.com>
2024-12-26 22:53:12 +0530

    Vector in C++

---
+++ c++_lec-14/vector.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() 
+{
+    vector<int> vec1 = {1,3,5,4,3,2};
+    
+    for(int i=0; i<vec1.size(); i++) {
+      cout << vec1[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
