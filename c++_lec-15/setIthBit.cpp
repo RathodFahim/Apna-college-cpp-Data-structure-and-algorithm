cbca091aa08f24f093ac30f12b0204555d4c8d3f
Rathod <rathodrathod75850@gmail.com>
2025-05-13 22:31:54 +0530

    Set and Clear ith bit

--- c++_lec-15/setIthBit.cpp
+++ c++_lec-15/setIthBit.cpp
@@ -5,11 +5,17 @@ int setIthBit(int n, int i) {
     int bitmask = 1 << i;
     return (n | bitmask);
 }
+int clearIthBit(int n, int i) {
+    int bitmask = ~(1 << i);
+    return n & bitmask;
+}
 
 int main() {
     int n = 6; // Binary: 110
     int i = 3;
 
-    cout << setIthBit(n, i) << endl; // Output: 6 (Binary: 110)
+    cout << setIthBit(n, i) << endl; //output is 14 (Binary: 1110)
+    cout << clearIthBit(n, i) << endl; //output is 6 (Binary: 110)
+
     return 0;
 }
