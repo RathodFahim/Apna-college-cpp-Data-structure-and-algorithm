2fb4a5fe882a225992e0383ba7be8e0059848f6d
Rathod <rathodrathod75850@gmail.com>
2025-06-25 23:36:07 +0530

    Class in c++

--- c++_lec-16/class.cpp
+++ c++_lec-16/class.cpp
@@ -14,5 +14,11 @@ public:
 
 int main() {
     Student s1; // Creating an object of the Student class
-    cout << sizeof(s1) << endl; // Output the size of the object
+    // cout << sizeof(s1) << endl; // Output the size of the object
+    s1.name = "John Doe"; 
+    s1.cgpa = 8.5; 
+    s1.getPercentage(); 
+    cout << "Name: " << s1.name << endl; 
+    cout << "CGPA: " << s1.cgpa << endl; 
+    return 0; 
 }
